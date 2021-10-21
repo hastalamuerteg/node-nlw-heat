22afd148114ae1decf1c06bd07356c035629ce2b
hastalamuerteg <gabriel.vicent@hotmail.com>
2021-10-20 22:16:24 -0300

    init io

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import { router } from "./routes";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
+app.use(cors);
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
 app.use(express.json());
 app.use(router);
 
